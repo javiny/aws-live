46e808a05cb16d0ee6a32b56a4a2b9d548e3f582
javiny <yeohch-wp22@student.tarc.edu.my>
2025-12-09 15:35:21 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cdesi4a6kyf7.us-east-1.rds.amazonaws.com"
+customuser = "aws_yeohchonghang
+custompass = "Master123"
+customdb = "employee"
+custombucket = "yeohchonghang-employee"
+customregion = "us-east-1"
+
